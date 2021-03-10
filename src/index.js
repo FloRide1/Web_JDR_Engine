d3fda60418467e3374ccc99d404b9a669ef78ccc
FloRide1 <florian.reimat@orange.fr>
2021-03-10 15:31:30 +0100

    Just the base project, i will do the README later

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const element = <div> <h1> This is just a basic project</h1></div>
+// ========================================
+
+ReactDOM.render(
+	element,
+	document.getElementById('root')
+);
+
